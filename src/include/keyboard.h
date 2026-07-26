1d07d0102ff5f5fe6bf2ede989d6ec8f3b42f597
pintoto <ary.303erfani@gmail.com>
2026-07-26 11:31:08 +0330

    Create keyboard.h

---
+++ src/include/keyboard.h
@@ -0,0 +1,26 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include "kernel.h"
+
+#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
+
+#define KEY_ENTER 0x1C
+#define KEY_BACKSPACE 0x0E
+#define KEY_ESC 0x01
+#define KEY_SHIFT 0x2A
+#define KEY_CTRL 0x1D
+#define KEY_ALT 0x38
+#define KEY_CAPS 0x3A
+#define KEY_UP 0x48
+#define KEY_DOWN 0x50
+#define KEY_LEFT 0x4B
+#define KEY_RIGHT 0x4D
+
+void keyboard_init(void);
+char keyboard_read_char(void);
+uint8_t keyboard_read_scancode(void);
+int keyboard_key_pressed(void);
+
+#endif
