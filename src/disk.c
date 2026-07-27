0bf9698a74c0a179e81f6c511d8d48204ed58572
pintoto <ary.303erfani@gmail.com>
2026-07-27 10:09:24 +0330

    Create disk.c

---
+++ src/disk.c
@@ -0,0 +1,11 @@
+#include "include/disk.h"
+
+void read_sector(uint32_t sector, uint8_t* buffer) {
+    (void)sector;
+    (void)buffer;
+}
+
+void write_sector(uint32_t sector, const uint8_t* buffer) {
+    (void)sector;
+    (void)buffer;
+}
